18c77e44cd366f435a8a7daa2d0863357da08264
Syeda Ayesha <122458936+Syedaa-Ayesha@users.noreply.github.com>
2023-05-15 23:07:02 +0500

    Done Assignment 1 and 2

---
+++ app.js
@@ -0,0 +1,68 @@
+//-----------------------------------------------Alert----------------------------------------
+
+//Q #1
+//alert("hello world");
+
+//Q#2
+// alert("Error! Please enter a valid password");
+
+//Q#3
+//alert("Welcome to js land...\n Happy coding");
+
+//Q#4
+//alert("Welcome to js Land...");
+//alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+//Q#5
+// console.log(JavaScript Alert);
+// console.log(Hello... I can run JS through my web browser console );
+// console.log(fullName) 
+
+//Q#6
+
+
+//----------------------------------------------Chapter 2------------------------------------
+
+//VARIABLES FOR STRINGS
+
+//Q#1
+//var username;
+
+//Q#2
+//var myName ="Syed Ayesha"
+
+//Q#4
+//var studentName ="Syeda Ayesha";
+//var age ="21";
+//var profile ="Certified Mobile Application Development";
+//alert(studentName);
+//alert(age);
+//alert(profile);
+
+//Q#5
+
+//var abc ="PIZZA \nPIZZ \nPIZ \nPI \nP";
+//alert(abc);
+
+//Q#6
+
+// var a ="syedaaayesha";
+// var b ="923";
+// var c ="@gmail.com";
+// var email = a+b+c;
+// alert(email);
+
+//Q#7
+ //var book ="A smarter way to learn JavaScript";
+ //alert(book);
+
+
+//Q#8
+
+//var  browser ="Yah! I can write HTML content through JavaScript";
+//document.write(browser);
+
+//Q#9
+
+//var border ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(border);
